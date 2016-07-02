d5adf126161431abffb3b2d5602a202ba67938ec
王照柯 <wangzhaoke_01@163.com>
2016-07-02 11:31:48 +0800

    wok
    开始框架

---
+++ StarCloset/StarCloset/Controls/WelcomeVC/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  MaleClothingState
+//
+//  Created by Mac on 16/7/2.
+//  Copyright © 2016年 王照柯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
---
+++ StarCloset/StarCloset/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StarCloset
+//
+//  Created by Mac on 16/7/2.
+//  Copyright © 2016年 王照柯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
